ca4be35a2642c807283ba1a25753f17496e0e1d7
Faety <faety@protonmail.ch>
2022-09-27 00:46:07 +0200

    modification des boutons

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require __DIR__ . '/config.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PassCrack</title>
+    <title>PassCrack - Accueil</title>
 
     <?php include __DIR__ . '/templates/header.html.php' ?>
 </head>
@@ -23,8 +23,8 @@ require __DIR__ . '/config.php';
     <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">PassCrack est une application gratuite et respectueuse de la vie privée qui a pour but de tester la complexité de vos mots de passe en tentant de les craquer. Toutes les interactions que vous pouvez avoir avec notre site ne sont pas analysées ou gardées sur nos serveurs.</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Soumettre votre mot de passe</button>
-            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+            <a type="button" class="btn btn-primary btn-lg px-4 gap-3" href="form.php">Soumettre votre mot de passe</a>
+            <a type="button" class="btn btn-outline-secondary btn-lg px-4" href="table.php">Tableau</a>
         </div>
     </div>
 </div>
