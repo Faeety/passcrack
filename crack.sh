c33826c703a98d76c5c993d109ddbc294493519c
Faety <faety@protonmail.ch>
2022-09-27 00:45:57 +0200

    Modification du script crack, le mot de passe false était considéré comme impossible

--- crack.sh
+++ crack.sh
@@ -19,11 +19,11 @@ hashcat --runtime 3600 -o cracked.txt -m 0 -a 3 $phash $patern
 # Si un fichier est crée alors on envoie le mot de passe craqué au serveur si non on dit au serveur que le hash est faux
 if [ -f "$FILE" ]; then
 	while read line; do
-		curl -d "id=$id&result=$line&token=$token" -X POST "https://passcrack.ch/scripts/handle_crack.php"
+		curl -d "id=$id&result=$line&cracked=true&token=$token" -X POST "https://passcrack.ch/scripts/handle_crack.php"
 		echo "$URL"
 	done < $FILE
 else
-	curl -d "id=$id&result=false&token=$token" -X POST "https://passcrack.ch/scripts/handle_crack.php"
+	curl -d "id=$id&cracked=false&token=$token" -X POST "https://passcrack.ch/scripts/handle_crack.php"
 fi
 
 cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > instance_id_hv; head -c -1 -q ssh_key_hv instance_id_hv > ~/.vast_api_key;
