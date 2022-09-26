a8ba5d322ef22af3f12091f7493dabba2065e23a
Faety <faety@protonmail.ch>
2022-09-27 00:49:23 +0200

    Ajout de deux pages

---
+++ table.php
@@ -0,0 +1,68 @@
+<?php
+
+require __DIR__ . '/config.php';
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PassCrack - Tableau</title>
+
+    <?php include __DIR__ . '/templates/header.html.php' ?>
+</head>
+
+<?php include __DIR__ . '/templates/navbar.html.php' ?>
+
+<body>
+
+<div class="container mb-5">
+    <div class="row justify-content-center mt-3">
+        <div class="col-md-8">
+            <div class="card bg-gradient-primary">
+                <div class="card-body text-center">
+                    <h2 class="display-4 text-white text-shadow mb-0">TABLEAU</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-8 mt-3">
+            <div class="card bg-faded-info">
+                <div class="card-body text-center">
+                    <h2 class="display-7 mb-0">25 dernières entrées dans la base de données</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-8 mt-3">
+            <div class="card bg-faded-primary">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Hash</th>
+                                <th>Result</th>
+                                <th>Status</th>
+                                <th>Temps</th>
+                            </tr>
+                            </thead>
+                            <?php echo $utility->TableConstruct($_SERVER['REMOTE_ADDR']); ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . '/templates/scripts.html.php' ?>
+
+</body>
+
+<?php include __DIR__ . '/templates/footer.html.php' ?>
+
+</html>
