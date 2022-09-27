0ef067f21d11b79365f567e53a0ce3d0155153a3
Faety <faety@protonmail.ch>
2022-09-27 20:07:58 +0200

    ajout de raison de die + limite à 9

--- scripts/add_password.php
+++ scripts/add_password.php
@@ -2,12 +2,16 @@
 
 require __DIR__ . "/../config.php";
 
-if (!isset($_POST['password'])) { http_response_code(400); die(); }
+if (!isset($_POST['password']) || empty($_POST['password'])) die("no password");
+if (!isset($_POST['type']) || empty($_POST['type'])) die("no type");
 
 $password = $_POST['password'];
+$hashtype = HashType::tryFrom($_POST['type']);
+if ($hashtype == null) die("wrong hashtype");
 $length = strlen($password);
-$hash = md5($password);
+$hash = hash($utility->GetHashName($hashtype), $password);
 
-if($length > 9) die();
+if($length > 9) die("too long"); // brute force en ?a impossible de faire plus
 
-$utility->SetPassword($utility->GetIdFromIP($_SERVER['REMOTE_ADDR']), $hash, $length);
+echo $utility->SetPassword($utility->GetIdFromIP($_SERVER['REMOTE_ADDR']), $hash, $length, $hashtype);
+die();
