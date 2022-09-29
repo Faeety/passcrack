f6e3657a3e0ccc6b986d727d8e4cda53b8073db9
Faety <faety@protonmail.ch>
2022-09-30 00:40:50 +0200

    adapatation pour acceuillir chiffrement asymétrique

--- scripts/add_password.php
+++ scripts/add_password.php
@@ -4,14 +4,16 @@ require __DIR__ . "/../config.php";
 
 if (!isset($_POST['password']) || empty($_POST['password'])) die("no password");
 if (!isset($_POST['type']) || empty($_POST['type'])) die("no type");
+if (!isset($_POST['pbkey']) || empty($_POST['pbkey'])) die("no key");
 
 $password = $_POST['password'];
 $hashtype = HashType::tryFrom($_POST['type']);
 if ($hashtype == null) die("wrong hashtype");
 $length = strlen($password);
 $hash = hash($utility->GetHashName($hashtype), $password);
+$pbkey = $_POST['pbkey'];
 
 if($length > 9) die("too long"); // brute force en ?a impossible de faire plus
 
-echo $utility->SetPassword($utility->GetIdFromIP($_SERVER['REMOTE_ADDR']), $hash, $length, $hashtype);
+echo $utility->SetPassword($utility->GetIdFromIP($_SERVER['REMOTE_ADDR']), $hash, $length, $hashtype, $pbkey);
 die();
