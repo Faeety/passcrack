8e7bdf56ba0a4927ff3e6fffc2cd929a6f7edccd
Faety <faety@protonmail.ch>
2022-09-30 00:37:36 +0200

    Modification pour chiffrement avec clé publique RSA

--- classes/Encryption.php
+++ classes/Encryption.php
@@ -2,17 +2,10 @@
 
 class Encryption
 {
-    public function Decrypt(string $ciphertext, string $key){
-        $c = base64_decode($ciphertext);
-        $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
-        $iv = substr($c, 0, $ivlen);
-        $hmac = substr($c, $ivlen, $sha2len=32);
-        $ciphertext_raw = substr($c, $ivlen+$sha2len);
-        $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
-        $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-        if (hash_equals($hmac, $calcmac))
-        {
-            echo $original_plaintext."\n";
-        }
+    public function Encrypt(string $message, string $pbkey){
+
+        openssl_public_encrypt($message, $encrypt, $pbkey, OPENSSL_PKCS1_OAEP_PADDING );
+
+        return base64_encode($encrypt);
     }
 }
