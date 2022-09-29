5941924d2e60960826d61afc9fb0896233b00235
Faety <faety@protonmail.ch>
2022-09-30 00:41:11 +0200

    modifications pour accueillir chiffrement asymétrique

--- scripts/handle_crack.php
+++ scripts/handle_crack.php
@@ -12,19 +12,20 @@ $cracked = $_POST['cracked'];
 # utile pour autre chose
 # SELECT password_status.name as name FROM password INNER JOIN password_status ON password.status = password_status.id WHERE status.id = :id
 
-$stmt = $conn->prepare("SELECT id FROM password WHERE id = :id");
+$stmt = $conn->prepare("SELECT id, pbkey FROM password WHERE id = :id");
 $stmt->bindParam(":id", $id, PDO::PARAM_INT);
 $stmt->execute();
 
 $row = $stmt->fetch();
 
 if (!$row) die("no row found with this id");
+$pbkey = preg_replace('/-----(\w+) (\w+) (\w+)-----/', "\n-----$1 $2 $3-----\n", $row["pbkey"]);
 
 if ($cracked == "false") {
     $utility->ChangePasswordStatus($row['id'], Status::IMPOSSIBLE);
 }else{
     $passwordArray = explode(':', $result);
-    $password = $passwordArray[1];
+    $password = $enc->Encrypt($passwordArray[1], $pbkey);
 
     $utility->ChangePasswordStatus($row['id'], Status::CRACKED, $password);
 }
