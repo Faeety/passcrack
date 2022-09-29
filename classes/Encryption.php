5010e7542f788423937a71735de24c3e63c643df
Faety <faety@protonmail.ch>
2022-09-30 00:36:24 +0200

    Ajout de méthodes de chiffrement avec une clé publique

---
+++ classes/Encryption.php
@@ -0,0 +1,18 @@
+<?php
+
+class Encryption
+{
+    public function Decrypt(string $ciphertext, string $key){
+        $c = base64_decode($ciphertext);
+        $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+        $iv = substr($c, 0, $ivlen);
+        $hmac = substr($c, $ivlen, $sha2len=32);
+        $ciphertext_raw = substr($c, $ivlen+$sha2len);
+        $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+        $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+        if (hash_equals($hmac, $calcmac))
+        {
+            echo $original_plaintext."\n";
+        }
+    }
+}
